d1bb6ddcabcd4ec5efcbcf61337c097a49e26b67
Lubnnia <lubnnia@gmail.com>
2023-11-23 16:06:27 -0300

    feat: Create styled component app and Header app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
 import type { AppProps } from 'next/app';
+import { globalStyles } from '../styles/global';
+
+import Image from 'next/image';
+
+import logoIgniteImg from '../assets/logo.svg';
+
+import { Container, Header } from '../styles/pages/app';
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoIgniteImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
