6f2fd650e0a33d9bfc74f271b3780208b3b14466
Lubnnia <lubnnia@gmail.com>
2023-11-25 16:11:20 -0300

    feat: Added functionality carrossel in page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,59 @@
-import { styled } from '../styles';
+import Image from 'next/image';
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import { HomeContainer, Product } from '../styles/pages/home';
 
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-});
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+
+import camiseta01 from '../assets/camisetas/camiseta01.png';
+import camiseta02 from '../assets/camisetas/camiseta02.png';
+import camiseta03 from '../assets/camisetas/camiseta03.png';
 
 export default function Home() {
-  return <Button>Enviar</Button>;
+  const [slidesRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
+  return (
+    <HomeContainer ref={slidesRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta01} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta02} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta03} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta03} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
